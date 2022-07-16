b502e5da9278699b552aa704b4302ec2534f322e
iamjuzarantri <juzarantri786@gmail.com>
2022-07-16 18:49:15 +0530

    66  done

---
+++ Min Heap.cpp
@@ -0,0 +1,13 @@
+vector<int> minHeap(int n, vector<vector<int>>& q) {
+    priority_queue<int,vector<int>,greater<int>> pq;
+    vector<int> ans;
+    for(int i = 0; i < q.size(); i++){
+        if(q[i][0] == 0){
+            pq.push(q[i][1]);
+        }else{
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+    }
+    return ans;
+}
